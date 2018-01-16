45c5c602f2d9ddf7bd0301e1f36d61bd6f3c438e
SiLenT_Tz <shadow-jan@hotmail.de>
2018-01-16 14:52:52 +0100

    clock works

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-about',
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,28 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  //Variables
+  hours:any;
+  minutes:any;
+  seconds:any;
 
+  constructor(public navCtrl: NavController) {
+    //Use new Date to get the current System Time and safe it into the variables
+    this.hours = new Date().getHours();
+    this.minutes = new Date().getMinutes();
+    this.seconds = new Date().getSeconds();
+    console.log("Constructor");
+    this.getTheTime();
   }
 
+  //The method wich updates the variables every second
+  getTheTime(){
+    console.log("Get The Time");
+      this.hours = new Date().getHours();
+      this.minutes = new Date().getMinutes();
+      this.seconds = new Date().getSeconds();
+      setTimeout(() => {
+        this.getTheTime();
+      }, 1000);
+  }
 }
